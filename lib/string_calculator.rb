5ab66680892062ced3145a600c31b3ef395a77df
ashadev <akshara6315@gmail.com>
2025-01-10 23:50:24 +0530

    TDD for string calculator

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,16 @@ class StringCalculator
 	def add(numbers)
 		return 0 if numbers.empty?
 
+		if numbers.start_with?("//")
+			delimiter, numbers = numbers[2..].split("\n", 2)
+			num = numbers.split(delimiter).map(&:to_i)
+		else
+			num = numbers.gsub("\n", ",").split(",").map(&:to_i)
+		end
+
+		neg = num.select(&:negative?)
+		raise "Negative numbers are not allowed: #{neg.join(', ')}" unless neg.empty?
+
+		num.inject(0, :+)
 	end
 end
