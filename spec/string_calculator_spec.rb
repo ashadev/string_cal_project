c5fd60f5e320c887f74311b8a7c36726dc062402
ashadev <akshara6315@gmail.com>
2025-01-28 16:19:55 +0530

    Comma delimiter handling

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,9 +10,21 @@ RSpec.describe StringCalculator do
 	      expect(StringCalculator.add("1")).to eq(1)
 	    end
 
-	    it 'returns the sum of two numbers' do
-	      expect(StringCalculator.add("1,2")).to eq(3)
+	    context 'when comma is the delimiter' do
+	    	it 'returns the sum of two numbers' do
+		      expect(StringCalculator.add("1,2")).to eq(3)
+		    end
+
+		    it 'returns the sum of multiple numbers' do
+	  			expect(StringCalculator.add("1,2,3,4")).to eq(10)
+			end
 	    end
+
+		context 'when \n is the delimiter' do
+			it 'returns the sum of multiple numbers' do
+	  			expect(StringCalculator.add("1\n2,3")).to eq(6)
+			end
+		end 
 	end
 end
 
