eb0543074f90643f2d7d25ae889c11ec8816ebc1
ashadev <akshara6315@gmail.com>
2025-01-28 16:13:36 +0530

    Split given number with comma

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe StringCalculator do
 		it 'return 0 for empty string' do
 			expect(StringCalculator.new.add("")).to eq(0)
 		end
+
+		it 'returns the number itself for a single number' do
+	      expect(StringCalculator.add("1")).to eq(1)
+	    end
+
+	    it 'returns the sum of two numbers' do
+	      expect(StringCalculator.add("1,2")).to eq(3)
+	    end
 	end
 end
 
